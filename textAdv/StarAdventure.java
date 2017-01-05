eaa1a6f61c68913820410f53cdf340e65ff2daae
averyprofesssionalhub <mathewmacdonald-smith@192.168.1.102>
2017-01-05 18:47:28 -0500

    as

--- textAdv/StarAdventure.java
+++ textAdv/StarAdventure.java
@@ -3,13 +3,11 @@
 import java.io.Serializable;
 import java.util.Scanner;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-import com.sun.webkit.ThemeClient;
 
 public class StarAdventure implements Serializable {
 
 	static Scanner in = new Scanner(System.in);
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		
 		expo();
 		
@@ -31,32 +29,36 @@ public class StarAdventure implements Serializable {
 	//method
 	}
 	//done
-	public static void expo() {
-		//beginning
-				System.out.println("You are floating through the stars in an escape chamber, you look at your holo-display to see an old barge."); 
+	public static void expo() throws InterruptedException {
+				System.out.println("You are floating through the stars in an escape pod, you look at your holo-display to see an old barge."); 
+				Thread.sleep(2000);
 				System.out.println("As you go toward it you notice it has no garbage on it. "
 				+ "While floating by, the ship drags you toward its bay doors.");
-				System.out.println("Once you are in the ship, your chamber is surrounded by people,you have seen their outfits before.");
-				System.out.println("You open your door to silence, the people are staring at you and after you exit the chamber, wisk you away to the crew deck.");
-				System.out.println("'Where are you from?', the captain says, you have no clue who you are or where you came from.");
-				System.out.println("As you look down, you notice you have a familiar uniform, but it has been town and it looks vandalised. ");
+				Thread.sleep(2000);
+				System.out.println("Once you are in the ship, your pod is surrounded by people. You have seen their outfits before but can't place where they are from.");
+				System.out.println("You open your door to silence. These people are staring at you in shock. After you exit the pod, they wisk you away to the crew deck.");
+				Thread.sleep(2000);
+				System.out.println("'Where are you from?', the captain says. You however, have no clue who you are or where you came from.");
+				System.out.println("As you look down, you notice you too have a familiar uniform, but it is different and has been wrecked. ");
 				System.out.println("'I don't know' you say.");
-				System.out.println("'oh, they must have wiped your memory', he says,'Hi, I am Steven and I captain this ship, what is your name?' ");
-				//player enters name
+				Thread.sleep(2000);
+				System.out.println("'Oh, they must have wiped your memory', he says,'Hi, I am Steven and I captain this ship, what is your name?' ");
+		 
+		 //player enters name
 				String name = in.nextLine();
 				System.out.println("'Hello, " + name + " welcome aboard the 'Tater' Steven says.");
-				System.out.println("Steven walks you throughout the 'Tater, it is a class 3 barge Ship that is built to fly under the radar, it has plenty of hi-tech systems though.");
-				System.out.println("you notice your systems from the chamber being added to the ships navigation systems and screen.");
+				System.out.println("Steven walks you throughout the 'Tater, it is a class 3 barge Ship that is built to fly under the radar though it has been outfitted nicely with gadgets.");
+				System.out.println("You notice your systems from the pod are being added to the ships navigation systems and screen already.");
 	}
 	//done
 	public static void engpil() {
-		System.out.println("you take control of the ship from the engine room, immediately the memories of flying ships come back to you");
-		System.out.println("you perform multiple acrobatic loops in the air and finally roll the ship a few times");
-		System.out.println("Steven Runs down to the engine room where he sees you at the controls'what were you doing?' he yells at you, clearly angry");
-	System.out.println("what do you say, 1: I was performing advanced maneuveres. or 2: I don't know. please type either 1 or 2 ");
+		System.out.println("You take control of the ship from the engine room, you seemingly feel like you have done this before.");
+		System.out.println("You perform multiple acrobatic loops in the air and finally roll the ship a few times.");
+		System.out.println("Steven runs down to the engine room where he sees you at the controls. 'What were you doing?' he yells at you, clearly angry.");
+	System.out.println("What do you say, 1: I was performing advanced maneuveres. or 2: I don't know. Please type either 1 or 2.");
 	int EngDes = in.nextInt();
 	if(EngDes == 1){
-		System.out.println("clearly shocked, Steven says'come with me to the cockpit'");
+		System.out.println("Clearly shocked, Steven says'come with me to the cockpit.'");
 		pilot();
 	}else{
 		System.out.println("Steven, clearly angry, throws you out of the ship and into open space where you die.");
@@ -66,15 +68,15 @@ public class StarAdventure implements Serializable {
 	}
 	//done
 	public static void pilot(){
-		System.out.println("Steven walks you towards the cockpit, 'try flying the ship', he says as he sits you at the primary controls");
-		System.out.println("as soon as you sit in the chair, your memories come back to you, you immediately pick up the speed and do acrobatic maneuveres in the barge");
-		System.out.println("everyone not already strapped in is flung across the ship and several technichians are very angry.");
-		System.out.println("steven asks you to pilot to coordinates '332, 564, 896'. type them in to warp to star harbour.");
+		System.out.println("Steven walks you towards the cockpit, 'Try flying the ship', he says as he sits you at the primary controls.");
+		System.out.println("As soon as you sit in the chair, your memories come back to you, you immediately pick up the speed and do acrobatic maneuveres in the barge");
+		System.out.println("Everyone not already strapped in is flung across the ship and several technichians are very angry.");
+		System.out.println("Steven asks you to pilot to coordinates '332, 564, 896'. type them in to warp to star harbour.");
 		String coord = in.nextLine();
 		//coordinates
 		if(coord.equals("332, 564, 896")){
 			System.out.println("you successfully entered the coordinates and landed safely at star harbour.");
-			System.out.println("getting out of the barge, you see the city of far harbour. It is a bustling trade city with few tall buildings and mostly small shacks where people buy and sell anything");
+			System.out.println("getting out of the barge, you see the city of star harbour. It is a bustling trade city with few tall buildings and mostly small shacks where people buy and sell anything");
 			System.out.println("it looks like a market on a much larger scale.");
 			System.out.println("do you go to the food or the items");
 			String market = in.nextLine();
@@ -111,7 +113,19 @@ public class StarAdventure implements Serializable {
 		System.out.println("YOU DIED. THE END, THANKS FOR PLAYING");
 	}
 	//not done
-	public static void items(){}
+	public static void items(){
+		System.out.println("Walking through the market, you spot a guy selling bombs. At just 2 credits a piece, they are a steal.");
+		System.out.println("you spend 10 credits to get 5 bombs, you continue to walk through the market.");
+		System.out.println("You get to the middle of the market. Just as you do, five cloaked figures, each with a similar logo to the one you had, show up.");
+		System.out.println("do you 1: throw your bombs at them. or 2: run away.");
+		int markBom = in.nextInt();
+		if(markBom == 1){
+			bombs();
+		}else{
+			running();
+		}
+	
+	}
 	//done
 	public static void janitor(){
 
@@ -178,6 +192,24 @@ public class StarAdventure implements Serializable {
 			}else{
 			}
 		}
+	}
+	public static void bombs(){
+		System.out.println("It all happened so fast, one moment you had the bombs in your hands.");
+		System.out.println("And the next they were at the feet of some soldiers.");
+		System.out.println("Though you decided to run away before the explosion, you could still loudly hear the bombs go off.");
+		System.out.println("With the bombs going off, chaos began in the market.");
+		System.out.println("people were running, fighting and yelling.");
+		System.out.println("You got lost in the crowd and almost ran into an officer but you luckily made your way back to the ship.");
+		System.out.println("you win");
+		
+	}
+	public static void running(){
+		System.out.println("The figures notice you immediately and start running towards you.");
+		System.out.println("They are nuch faster than you and are quickly on your tail.");
+		System.out.println("To get them off your case, you throw a bomb at them.");
+		System.out.println("this seems to work but only because they are getting killed.");
+		System.out.println("you finally make your way back to the ship without the figures");
+		System.out.println("you win");
 	}
 	private static final long serialVersionUID = 1L;
 }
